4ac7a9b0ab0f244cef277f154c4e94ed12d22418
“ealwaafy” <ealwaafy@gmail.com>
2022-07-19 09:48:38 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * set_string - Sets the value of a pointer to a char.
  * @s: The pointer.
  * @to: The char.
  */
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
+}
+int main(void)
+{
+	char *s0 = "Bob Dylan";
+	char *s1 = "Robert Allen";
+
+	printf("%s, %s\n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s\n", s0, s1);
+	return (0);
 }
