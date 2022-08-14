d9e07831ee005b20f54d468bad3bd33556f05ad4
“ealwaafy” <ealwaafy@gmail.com>
2022-08-14 07:29:19 -0700

    fixing 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,30 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
-
+#include "variadic_functions.h"
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * sum_them_all - the sum of all its parameters
+ * @n: number of parameters received
  *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * Return: 0 if n is 0 of the sum of parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	unsigned int i;
+	int add = 0, number;
 
-	va_start(nums, n);
+	va_list parameters;
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-	va_end(nums);
-	return (sum);
+	va_start(parameters, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		number = va_arg(parameters, int);
+		add = add + number;
+	}
+	va_end(parameters);
+	return (add);
 }
