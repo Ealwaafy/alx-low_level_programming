18ba0b6bd7386ac57894cfe85dd13dcc1adb7c29
“ealwaafy” <ealwaafy@gmail.com>
2022-08-21 11:55:42 -0700

    fixing 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_binary - prints the binary representation of a number
  * @n: a string of 0 and 1 chars
@@ -9,7 +9,6 @@ void print_binary(unsigned long int n)
 	int printbin = ((n & 1) + '0');
 
 	if (n > 1)
-	
 		print_binary(n >> 1)
 }
 _putchar(printbin);
